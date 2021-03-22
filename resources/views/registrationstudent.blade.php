d3aab4d50cb37892c82d0b5983ad60a29b749d5f
Hakim Danial <rylaiftw@gmail.com>
2021-03-22 09:16:25 +0800

    1.3

--- resources/views/registrationstudent.blade.php
+++ resources/views/registrationstudent.blade.php
@@ -205,17 +205,9 @@
     
 
     <div class="container">
-      <div class="copyright">
-        &copy; Copyright <strong><span>Versatile Straits Sdn. Bhd</span></strong>. All Rights Reserved
-      </div>
-     <!-- <div class="credits"> -->
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/flexstart-bootstrap-startup-template/ -->
-        
-      </div>
-    
+        <div class="copyright">
+          &copy; Copyright <strong><span>Versatile Straits Sdn. Bhd</span></strong>. All Rights Reserved
+        </div>    
     </div>
   </footer><!-- End Footer -->
 
