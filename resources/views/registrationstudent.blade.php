b7b970ac0f91ce8e0e810c1a99ecc448a31350fe
Fadli Saad <fadlisaad@gmail.com>
2021-03-29 16:26:16 +0800

    Add missing form multipart
    Signed-off-by: Fadli Saad <fadlisaad@gmail.com>

--- resources/views/registrationstudent.blade.php
+++ resources/views/registrationstudent.blade.php
@@ -41,7 +41,7 @@ Student Registration
                     {{ session()->get('success') }}
                 </div>
             @endif
-                  <form id="survey-form" data-aos="zoom-in" data-aos-delay="100" class="register" method="post" action="{{ route('create-student') }}">
+                  <form id="survey-form" data-aos="zoom-in" data-aos-delay="100" class="register" method="post" action="{{ route('create-student') }}" enctype="multipart/form-data">
                     @csrf
                     <div class="form-group">
                       <label id="fullname-label" for="fullname">Full Name</label>
