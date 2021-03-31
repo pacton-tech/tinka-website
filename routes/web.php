9046cce4b8091e9bad58cd8c23619ca7cd1b117a
Fadli Saad <fadlisaad@gmail.com>
2021-03-31 13:06:36 +0800

    Update route for registration
    Signed-off-by: Fadli Saad <fadlisaad@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -59,8 +59,14 @@ Route::get('/teachers', function() {
     return view('teachers');
 });
 
-Route::get('/register/teacher', 'TeacherController@create')->name('register-teacher');
-Route::get('/register/student', 'StudentController@create')->name('register-student');
+Route::get('/register/teacher', function() {
+    return view('coming-soon');
+});
+Route::get('/register/student', function() {
+    return view('coming-soon');
+});
+//Route::get('/register/teacher', 'TeacherController@create')->name('register-teacher');
+//Route::get('/register/student', 'StudentController@create')->name('register-student');
 
 Route::post('/create/teacher', 'TeacherController@store')->name('create-teacher');
 Route::post('/create/student', 'StudentController@store')->name('create-student');
