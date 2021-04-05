678a85631ed73ef8dea12f9be89b5a9cb2856f16
pijut <80872121+pijut@users.noreply.github.com>
2021-04-05 17:27:40 +0800

    Update index.blade.php

--- resources/views/faqs/index.blade.php
+++ resources/views/faqs/index.blade.php
@@ -1,18 +1,24 @@
 @extends('faqs.layout')
  
  @section('content')
-     <div class="row" 
-            style=" box-sizing: border-box;
-                    margin: 20px;
-                    justify-content: center;">
+        <div class="row" 
+            style=" box-sizing: border-box; justify-content: center; margin-top:54px;">
          <div class="col-lg-12 margin-tb">
-             <div class="pull-left">
-                 <h2>FAQ Admin Interface</h2>
-             </div>
-             <div class="pull-right">
-                 <a class="btn btn-success" href="{{ route('faqs.create') }}"> Create New FAQ</a>
-             </div>
+            <div class="text-center">
+                <h2>FAQ Admin Interface</h2>
+            </div>
          </div>
+     </div>
+     <div class="row"style=" box-sizing: border-box; margin-bottom:20px; margin-top:50px; justify-content: center;">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <a class="btn btn-primary" href="{{ route('faqs.create') }}"> Create New FAQ</a>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('faqcategories.index') }}"> View Categories</a>
+                 <a class="btn btn-primary" href="{{ route('faqsubcategories.index') }}"> View Subcategories</a>
+            </div>          
+        </div>
      </div>
     
      @if ($message = Session::get('success'))
@@ -20,41 +26,29 @@
              <p>{{ $message }}</p>
          </div>
      @endif
-    
-    <div class="dropdown">
-      <button class="dropbtn">Category</button>
-        <div class="dropdown-content">
-            <a href="#">Category 1</a>
-            <a href="#">Category 2</a>
-            <a href="#">Category 3</a>
-        </div>
-    </div>
 
     <table class="table table-bordered" 
-            style=" justify-content: center;
-                    margin: 20px;">
+            style=" justify-content: center;">
          <tbody>
          <tr>
-             <th>No</th>
+             <th width="40px" style="text-align:center;">No</th>
              <th>Question</th>
              <th>Answer</th>
-             <th>Category</th>
-             <th>Subcategory</th>
-             <th width="280px">Action</th>
+             <th width="90px" style="text-align:center;">Category</th>
+             <th width="100px" style="text-align:center;">Subcategory</th>
+             <th width="100px" style="text-align:center;">Action</th>
          </tr>
          @foreach ($faqs as $faq)
          <tr>
-             <td>{{ ++$i }}</td>
+             <td  style="text-align:center;">{{ ++$i }}</td>
              <td>{{ $faq->question }}</td>
              <td>{{ $faq->answer }}</td>
-             <td>{{ $faq->categoryid }}</td>
-             <td>{{ $faq->subcategoryid }}</td>
+             <td  style="text-align:center;">{{ $faq->categories['categoryname'] }}</td>
+             <td  style="text-align:center;">{{ $faq->subcategories['subcategoryname'] }}</td>
              <td>
                  <form action="{{ route('faqs.destroy',$faq->id) }}" method="POST">
-    
-                     <a class="btn btn-info" href="{{ route('faqs.show',$faq->id) }}">Show</a>
      
-                     <a class="btn btn-primary" href="{{ route('faqs.edit',$faq->id) }}">Edit</a>
+                     <a class="btn btn-primary" style="margin-bottom:5px;" href="{{ route('faqs.edit',$faq->id) }}">&nbsp&nbspEdit&nbsp&nbsp</a>
     
                      @csrf
                      @method('DELETE')
