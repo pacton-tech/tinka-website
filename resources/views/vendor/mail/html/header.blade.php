fe1c5afa8ef393177b7473b5f049f825363237d9
Fadli Saad <fadlisaad@gmail.com>
2021-07-06 16:17:18 +0800

    Update logo
    Signed-off-by: Fadli Saad <fadlisaad@gmail.com>

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,11 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
 <img src="https://tinka.world/assets/img/tinka-logo.png" class="logo" alt="App Logo">
-@else
-{{ $slot }}
-@endif
 </a>
 </td>
 </tr>
