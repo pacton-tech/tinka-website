dc365941a043323ba4eb83645a61d25f18c4c9f6
ThatGuy <harithkhairol@gmail.com>
2022-01-28 09:29:54 +0800

    update tinka campus picture

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -94,7 +94,7 @@ TiNKA - Education. Everywhere.
         <div class="row gx-0">
           <div class="col-lg-6 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="200">
             <!-- <img src="assets/img/apps.png" class="img-fluid" alt=""> -->
-            <img src="assets/img/onlinelearning.jpg" class="img-fluid" alt="">
+            <img src="assets/img/onlinelearning.png" class="img-fluid" alt="">
           </div>
           <div class="col-lg-6 d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="200">
             <div class="content">
