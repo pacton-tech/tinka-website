ba6ca6d69af6a3ffe690c520b7d5def5467ee5b7
Fadli Saad <fadlisaad@gmail.com>
2022-05-18 11:54:54 +0800

    Fix banner

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,30 +11,6 @@ TiNKA - Education. Everywhere.
  <section class="d-flex align-items-center">
     <div class="container">
       <div class="row">
-        <div class="col-lg-5 d-flex flex-column justify-content-center">
-            <h1 data-aos="fade-up">{{ __("Join Us") }}</h1>
-            <h2 data-aos="fade-up" data-aos-delay="400">Open Day Raya TiNKA</h2>
-            
-            <div data-aos="fade-up" data-aos-delay="600">
-              <div class="text-center text-lg-start">
-<!--                 <a href="https://forms.gle/3NH2XQAhEU4mSoeb7" class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
- -->                  <span>{{ __("Register Now") }}</span>
-                  <i class="bi bi-arrow-right"></i> -->
-                </a>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-5 hero-img" data-aos="zoom-out" data-aos-delay="200">
-            <img src="{{ asset('uploads/slider/rayaceleb.jpeg') }}" class="img-fluid" alt="raya celebration">
-          </div>
-
-      </div>
-    </div>
-  </section>
-  <section class="d-flex align-items-center">
-    <div class="container">
-      <div class="row">
         <div class="col-lg-6 d-flex flex-column justify-content-center">
             <h1 data-aos="fade-up">Tinka Campus</h1>
             <h2 data-aos="fade-up" data-aos-delay="400">
